93840e8842ae3160dd2f686bb77353b9c92d955f
hwasub216 <47962357+hwasub216@users.noreply.github.com>
2019-04-24 23:08:04 +0900

    3.1 making a js clock part one

--- index.js
+++
@@ -1,8 +0,0 @@
-const title = document.querySelector("#title");
-
-function handleClick() {
-    title.style.color = "blue";
-}
-
-title.addEventListener("click", handleClick) 
-//handleResize() 는 즉시 호출할때
