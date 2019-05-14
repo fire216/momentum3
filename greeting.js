98c6c96a4ab22d6a6be9cbf4257fe1cd07b66ba2
hwasub216 <47962357+hwasub216@users.noreply.github.com>
2019-05-14 23:13:15 +0900

    3.8 Image background finished

--- greeting.js
+++ greeting.js
@@ -30,7 +30,7 @@ const USER_LS = "currentUser",
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `Hello ${text} !`;
 }
 
 function loadName(){
